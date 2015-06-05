f17b7b3ba6dfe1d5763ff71255687a4d31720af2
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2015-06-05 11:32:00 +0200

    recognising value attribute

---
+++ rog-publicsearches/rog-publicsearches-d2-api/src/main/java/com/emc/rogetmf/webfs/ui/assistance/DQLTemplateUtils.java
@@ -0,0 +1,100 @@
+package com.emc.rogetmf.webfs.ui.assistance;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.StringUtils;
+
+import com.emc.d2fs.dctm.ui.DialogProcessor;
+
+public class DQLTemplateUtils {
+
+	public static final String ATTR_EVALUATE_ON_INIT = "evaluateOnInit";
+
+	// id of control may consist of a-Z, 0-9, -, and _
+	public static final String HIDDEN_FIELD_SUFIX_SEPARATOR = "_-_";
+	
+	public static final String PARAM_LIST = "list";
+
+	/**
+	 * Checks if a hidden control has dependencies. This can be only evaluated during property page initialization
+	 * 
+	 * @param attributes
+	 * @return
+	 */
+	public static boolean hasNoDependencies(Map<String, Object> attributes) {
+		String dependencies = (String) attributes.get(DialogProcessor.ATTR_DEPENDENCIES);
+
+		// dependencies attribute is passed during control initialization only,
+		// therefore lack of the attribute in the map doesn't mean that control
+		// has no dependencies
+		if (dependencies == null)
+			return false;
+
+		// this situation should not happen, as a dynamic control will always
+		// contain reference to hidden dql_template field
+		if (StringUtils.isBlank(dependencies))
+			return true;
+
+		String[] dependenciesArray = StringUtils.split(dependencies, ",");
+		if (dependenciesArray.length > 0) {
+			// if list of dependencies contains nothing but ids of hidden fields,
+			// than we consider the control to have no dependencies
+			// hidden fields contain _-_[parent_control] suffix
+			for (int i = 0; i < dependenciesArray.length; i++)
+				if (!StringUtils.contains(dependenciesArray[i], HIDDEN_FIELD_SUFIX_SEPARATOR))
+					return false;
+
+			boolean asynchronous = BooleanUtils.toBoolean((String) attributes.get(DialogProcessor.ATTR_ASYNCHRONOUS));
+			if (asynchronous)
+				// LOGGER.warn("Asynchronous control with no dependencies found. The assistance DQL will be evaluated twice.");
+
+				// all dependencies contained "_-_" - this means the dynamic control has no dependencies
+				return true;
+		}
+
+		return false;
+
+	}
+
+	public static String getHiddenControlValue(Map<String, Object> attributes, String controlId) {
+		// check if control has a preset value or ATTR_EVALUATE_ON_INIT is set to true
+		boolean evaluateOnInit = attributes.containsKey(DialogProcessor.ATTR_VALUE)
+				|| BooleanUtils.toBoolean((String) attributes.get(ATTR_EVALUATE_ON_INIT));
+		return getHiddenControlValue(attributes, controlId, evaluateOnInit);
+	}
+
+	/**
+	 * Returns value of hidden control. Hidden controls are used to support evaluation of options for a dynamic control. If the dynamic
+	 * control has dependencies (other controls which impact it's value) and property page is being initialized, this method will return
+	 * null.
+	 * 
+	 * @param attributes
+	 * @return
+	 */
+	public static String getHiddenControlValue(Map<String, Object> attributes, String controlId, boolean evaluateOnInit) {
+		// when the dialog is initialized, non standard attributes are included in the attributes map
+		String controlValue = (String) attributes.get(controlId);
+
+		// if list of is empty we can return the value of hidden control during property page initialization
+		if (evaluateOnInit || hasNoDependencies(attributes))
+			return controlValue;
+
+		// otherwise, if control has dependencies, first the properties screen needs to be initialized
+		// controlValue is only null after property page initialization
+		else if (controlValue == null) {
+			String controlIdSufixed = controlId + HIDDEN_FIELD_SUFIX_SEPARATOR;
+			Iterator<Entry<String, Object>> attributeIterator = attributes.entrySet().iterator();
+			while (attributeIterator.hasNext()) {
+				Entry<String, Object> attribute = attributeIterator.next();
+				if (attribute.getKey().startsWith(controlIdSufixed))
+					return (String) attribute.getValue();
+			}
+		}
+
+		return null;
+	}
+
+}
--- rog-publicsearches/rog-publicsearches-d2-plugin/src/main/java/com/emc/rogetmf/webfs/ui/dynamiccontrols/DQLTemplateControl.java
+++ rog-publicsearches/rog-publicsearches-d2-plugin/src/main/java/com/emc/rogetmf/webfs/ui/dynamiccontrols/DQLTemplateControl.java
@@ -18,6 +18,7 @@ import com.emc.d2fs.dctm.exceptions.D2WarningException;
 import com.emc.d2fs.dctm.ui.DialogProcessor;
 import com.emc.d2fs.dctm.ui.dynamiccontrols.IDynamicControls;
 import com.emc.d2fs.dctm.web.services.D2fsContext;
+import com.emc.rogetmf.webfs.ui.assistance.DQLTemplateUtils;
 
 /**
  * Custom dynamic control which values are provided by a DQLs with FreeMarker syntax. This is a generic class, which can generate any type
@@ -30,7 +31,7 @@ public final class DQLTemplateControl implements IDynamicControls {
 	private Logger LOGGER = LoggerFactory.getLogger(getClass());
 
 	public static final String ATTR_CONTROL = "control";
-	
+
 	public static final String ATTR_CONTROL_TYPE = "control_type";
 
 	public static final String ATTR_DQL_TEMPLATE = "dql_template";
@@ -38,14 +39,9 @@ public final class DQLTemplateControl implements IDynamicControls {
 	public static final String ATTR_LABEL_TEMPLATE = "label_template";
 
 	public static final String ATTR_LABEL_TEMPLATE_ATTRS = "label_template_attrs";
-	
-	public static final String ATTR_EVALUATE_ON_INIT = "evaluateOnInit";
-	
-	// id of control may consist of a-Z, 0-9, -, and _
-	public static final String HIDDEN_FIELD_SUFIX_SEPARATOR = "_-_";	
-	
-	private static final String LIST_SEPARATOR = ",";	
-	
+
+	private static final String LIST_SEPARATOR = ",";
+
 	private ArrayList<String> hiddenControlIds = new ArrayList<String>();
 
 	public XmlNode getXmlControls(D2fsContext context, MultiResourceBundle labelsBundle, @SuppressWarnings("rawtypes") Map attributes)
@@ -75,31 +71,26 @@ public final class DQLTemplateControl implements IDynamicControls {
 			if (attributes.containsKey(ATTR_LABEL_TEMPLATE_ATTRS))
 				// set the label template attributes for the generated control
 				appendHiddenTextControl(result, attributes, ATTR_LABEL_TEMPLATE_ATTRS, (String) attributes.get(ATTR_LABEL_TEMPLATE_ATTRS));
-			
+
 			if (StringUtils.isBlank((String) attributes.get(DialogProcessor.ATTR_DEPENDENCIES)))
 				listXmlNode.setAttribute(DialogProcessor.ATTR_DEPENDENCIES, StringUtils.join(hiddenControlIds, LIST_SEPARATOR));
 			else
 				listXmlNode.setAttribute(DialogProcessor.ATTR_DEPENDENCIES, attributes.get(DialogProcessor.ATTR_DEPENDENCIES) + ","
 						+ StringUtils.join(hiddenControlIds, LIST_SEPARATOR));
-			
-			// if control is bounded to an attribute ("linked to property"), pass the value of the attribute
-			// in this case "control" attribute is missing or set to false
-			if(!attributes.containsKey(ATTR_CONTROL) || !BooleanUtils.toBoolean((String)attributes.get(ATTR_CONTROL))) {
-				
-				// check if the id of control is set (name of the attribute)
-				if(attributes.containsKey(DialogProcessor.ATTR_ID)) {
-					String attributeName = (String) attributes.get(DialogProcessor.ATTR_ID);
-					
-					IDfPersistentObject object = context.getFirstObject();
-					// check if property page is bounded to an object
-					if(object != null) {
+
+			// check if the id of control is set (name of the attribute)
+			if (attributes.containsKey(DialogProcessor.ATTR_ID)) {
+				String attributeName = (String) attributes.get(DialogProcessor.ATTR_ID);
+				IDfPersistentObject object = context.getFirstObject();
+
+				if (object != null) {
+					if (!attributes.containsKey(ATTR_CONTROL) || !BooleanUtils.toBoolean((String) attributes.get(ATTR_CONTROL))) {
 						String attributeValue = object.getValue(attributeName).asString();
 						listXmlNode.setAttribute(DialogProcessor.ATTR_VALUE, attributeValue);
 					}
 				}
-				
 			}
-			
+
 			LOGGER.debug(result.toString(true));
 
 		} catch (NoClassDefFoundError e) {
@@ -115,12 +106,12 @@ public final class DQLTemplateControl implements IDynamicControls {
 
 	private XmlNode appendHiddenTextControl(XmlNode result, @SuppressWarnings("rawtypes") Map attributes, String controlId, String value) {
 		// concatenate given id with the id of the parent control, so that the hidden control has a unique id
-		controlId += HIDDEN_FIELD_SUFIX_SEPARATOR + attributes.get(DialogProcessor.ATTR_ID);
-		
+		controlId += DQLTemplateUtils.HIDDEN_FIELD_SUFIX_SEPARATOR + attributes.get(DialogProcessor.ATTR_ID);
+
 		// store the control id, so that we can add it later on as a dependency
-		// otherwise the value of the hidden control would be inaccessible in IJavaAssistance::getValue 
+		// otherwise the value of the hidden control would be inaccessible in IJavaAssistance::getValue
 		hiddenControlIds.add(controlId);
-		
+
 		XmlNode listSupportXmlNode = result.appendChildNode("text");
 		listSupportXmlNode.setAttribute(DialogProcessor.ATTR_ID, controlId);
 		// value is a standard argument, thus it is not erased
